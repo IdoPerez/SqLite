af6d4989054805073c324bf2667f797fa8569e62
IdoPerez <ap1597@bs.amalnet.k12.il>
2020-01-15 14:20:50 +0200

    hi

---
+++ app/src/main/java/Grades.java
@@ -0,0 +1,7 @@
+public class Grades {
+    public final String TABLE_GRADES= "Grades";
+    public final String MATH = "Math";
+    public final String ENGLISH = "English";
+    public final String CYBER = "Cyber";
+    public final String ALBERT = "Albert";
+}
---
+++ app/src/main/java/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    public final String TABLE_STUDENTS = "Students";
+    public final String KEY_ID = " id";
+    public final String NAME = "Name";
+    public final String ADRESS = "Adress";
+    public final String PHONE_NUM = "Phone_Num";
+    public final String HOME_NUM = "Home_Num";
+    public final String FATHER_NAME = "Father_Name";
+    public final String MOTHER_NAME = "Mother_Name";
+    public final String MOM_NUM = "Mother_Num";
+    public final String DAD_NUM= "Father_Num";
+
+}
