6bf778b8619afe4574c32d236fcbff57178bb95f
liemb <lb6031@bs.amalnet.k12.il>
2020-02-16 13:29:25 +0200

    ganiefes

--- app/src/main/java/Grades.java
+++
@@ -1,7 +0,0 @@
-public class Grades {
-    public final static String TABLE_GRADES= "Grades";
-    public final static String MATH = "Math";
-    public final static String ENGLISH = "English";
-    public final static String CYBER = "Cyber";
-    public final static String ALBERT = "Albert";
-}
